d7de77c339106fc5e079631f1c6a3275dd8164ba
Vlad-PyDev <frotermamal@gmail.com>
2026-06-09 19:44:26 +0200

    Add files via upload

---
+++ app/build/generated/source/buildConfig/androidTest/debug/com/example/polynation/test/BuildConfig.java
@@ -0,0 +1,14 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.polynation.test;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.polynation.test";
+  public static final String BUILD_TYPE = "debug";
+  // Field from default config.
+  public static final String TRAVELPAYOUTS_MARKER = "735168";
+  // Field from default config.
+  public static final String TRAVELPAYOUTS_TOKEN = "487988201b234cde2989acce61cab7cf";
+}
---
+++ app/build/generated/source/buildConfig/debug/com/example/polynation/BuildConfig.java
@@ -0,0 +1,16 @@
+/**
+ * Automatically generated file. DO NOT MODIFY
+ */
+package com.example.polynation;
+
+public final class BuildConfig {
+  public static final boolean DEBUG = Boolean.parseBoolean("true");
+  public static final String APPLICATION_ID = "com.example.polynation";
+  public static final String BUILD_TYPE = "debug";
+  public static final int VERSION_CODE = 1;
+  public static final String VERSION_NAME = "1.0";
+  // Field from default config.
+  public static final String TRAVELPAYOUTS_MARKER = "735168";
+  // Field from default config.
+  public static final String TRAVELPAYOUTS_TOKEN = "487988201b234cde2989acce61cab7cf";
+}
